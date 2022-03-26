1f89a6c39a418e6e2a5aa5f1038add4becd1d834
raych2 <rtongco@gmail.com>
2022-03-25 18:38:16 -0700

    Add component routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Tips from './components/Tips';
+import Stats from './components/Stats';
+import Quiz from './components/Quiz';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Rehydrate</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="tips" element={<Tips />} />
+        <Route path="stats" element={<Stats />} />
+        <Route path="quiz" element={<Quiz />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
