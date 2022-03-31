d090911190baba9e5d4f314d7fa44c6ba7b18687
raych2 <rtongco@gmail.com>
2022-03-30 20:48:34 -0700

    Add Link element to Home to navigate to About page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Tips from './components/Tips';
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,14 @@
-import logo from "../assets/Rehydrate_Final_white_small2.png";
+import { Link } from 'react-router-dom';
+import logo from '../assets/Rehydrate_Final_white_small2.png';
 
 const Home = () => {
   return (
     <div className="home">
       <h1 className="title">Rehydrate</h1>
       <img className="logo" alt="rehydrate logo" src={logo}></img>
-      <button
-        type="button"
-        className="quizLink"
-      >
-        Learn More!
-      </button>
+      <Link to="about">
+        <button type="button">Learn More!</button>
+      </Link>
     </div>
   );
 };
