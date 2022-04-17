e717fe92e040e9bf49d325fedcc2dfb16dab2250
raych2 <rtongco@gmail.com>
2022-04-16 21:10:10 -0700

    Update Tips component

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -1,5 +1,33 @@
 const Tips = () => {
-  return <div>Tips</div>;
+  return (
+    <>
+      <div className="topicSection smallerTopicSection">
+        <div className="mainHeader">
+          How to Save <div className="rectangle">Water</div>?
+        </div>
+        <div className="infoContainer">
+          <div className="subHeader">Take a quiz</div>
+          <div className="subText">
+            Test your knowledge on our website and record your score!
+          </div>
+        </div>
+        <div className="infoContainer">
+          <div className="subHeader">Read</div>
+          <div className="subText">
+            Rehydrate holds infographics updated with our research on saving
+            water and related topics.
+          </div>
+        </div>
+        <div className="infoContainer">
+          <div className="subHeader">Share</div>
+          <div className="subText">
+            Our infographics and your quiz score are shareable to social media.
+            Teach your peers what they can do to "Rehydrate!"
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Tips;
