eae21bb66975c166c538459346b32624a905f032
raych2 <rtongco@gmail.com>
2022-04-17 13:46:24 -0700

    Update Stats component

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,5 +1,35 @@
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <>
+      <div className="topicSection smallerTopicSection">
+        <div className="mainHeader">
+          How to Access <div className="rectangle">Stats</div>?
+        </div>
+        <div className="infoContainer">
+          <div className="subHeader">Annual Reports</div>
+          <div className="subText">
+            Corporations are obligations to publish annual reports. Because of
+            the climate crisis and the rising trend of "sustainability," the
+            reports have increasingly included environmental policies,
+            practices, and charities.
+          </div>
+        </div>
+        <div className="infoContainer">
+          <div className="subHeader">Sustainable Practices</div>
+          <div className="subText">
+            Corporations have published non-financial reports to state their
+            environmental and social performances. If some companies do not have
+            these, then we will dig into their financial reports on water usage
+            tied to the workplace and suppliers.
+          </div>
+          <div className="subText">
+            Here are some key words: "community service", "social impact",
+            "water management", "water usage"
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Stats;
