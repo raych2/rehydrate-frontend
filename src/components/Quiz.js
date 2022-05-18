6c58ed48424bc7b7b011a2bcf985b7b0a25a64c4
raych2 <rtongco@gmail.com>
2022-05-18 14:49:57 -0700

    Fetch questions from quiz api and set questions state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,35 @@
+import { useState, useEffect } from 'react';
+
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function getQuizQuestions() {
+      try {
+        const response = await fetch(
+          'https://rehydrate-quiz.herokuapp.com/questions'
+        );
+        const data = await response.json();
+        const { questions } = data;
+        setQuestions(questions);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getQuizQuestions();
+  }, []);
+
+  return (
+    <>
+      <div className="topicSection smallerTopicSection">
+        <div className="mainHeader">
+          <div className="rectangle" id="quiz">
+            Quiz
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Quiz;
