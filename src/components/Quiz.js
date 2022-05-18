0cdc8ca885b95f6c64c6cd4ab5c830db396d31ee
raych2 <rtongco@gmail.com>
2022-05-18 14:58:30 -0700

    Add score, first question, and answer choices

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     async function getQuizQuestions() {
@@ -26,6 +28,33 @@ const Quiz = () => {
           <div className="rectangle" id="quiz">
             Quiz
           </div>
+        </div>
+        <div className="questionContainer">
+          <h2>Score: {score}/5</h2>
+          <h2 className="instructions">Please select the best answer.</h2>
+          {questions
+            .filter(
+              (question) =>
+                questions.indexOf(question) === currentQuestionNumber
+            )
+            .map((question, index) => {
+              return (
+                <div key={question._id}>
+                  <h3>
+                    {currentQuestionNumber + 1}. {question.questionText}
+                  </h3>
+                  <div className="answerContainer">
+                    {question.answerOptions.map((answer, index) => {
+                      return (
+                        <button key={answer._id} className="answer">
+                          {answer.answerText}
+                        </button>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
