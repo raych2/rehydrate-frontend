9881089309a9206a672b8213dce6d4bac047f526
raych2 <rtongco@gmail.com>
2022-05-19 14:42:59 -0700

    Add loadingMessage and errorMessage states

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,10 +4,13 @@ const Quiz = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
   const [score, setScore] = useState(0);
+  const [loadingMessage, setLoadingMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   useEffect(() => {
     async function getQuizQuestions() {
       try {
+        setLoadingMessage(true);
         const response = await fetch(
           'https://rehydrate-quiz.herokuapp.com/questions'
         );
@@ -16,46 +19,53 @@ const Quiz = () => {
         setQuestions(questions);
       } catch (err) {
         console.log(err);
+        setErrorMessage(true);
       }
+      setLoadingMessage(false);
     }
     getQuizQuestions();
   }, []);
 
   return (
     <>
+      {errorMessage && <p>An error has occured</p>}
       <div className="topicSection smallerTopicSection">
         <div className="mainHeader">
           <div className="rectangle" id="quiz">
             Quiz
           </div>
         </div>
-        <div className="questionContainer">
-          <h2>Score: {score}/5</h2>
-          <h2 className="instructions">Please select the best answer.</h2>
-          {questions
-            .filter(
-              (question) =>
-                questions.indexOf(question) === currentQuestionNumber
-            )
-            .map((question, index) => {
-              return (
-                <div key={question._id}>
-                  <h3>
-                    {currentQuestionNumber + 1}. {question.questionText}
-                  </h3>
-                  <div className="answerContainer">
-                    {question.answerOptions.map((answer, index) => {
-                      return (
-                        <button key={answer._id} className="answer">
-                          {answer.answerText}
-                        </button>
-                      );
-                    })}
+        {loadingMessage ? (
+          <h2>Loading ...</h2>
+        ) : (
+          <div className="questionContainer">
+            <h2>Score: {score}/5</h2>
+            <h2 className="instructions">Please select the best answer.</h2>
+            {questions
+              .filter(
+                (question) =>
+                  questions.indexOf(question) === currentQuestionNumber
+              )
+              .map((question, index) => {
+                return (
+                  <div key={question._id}>
+                    <h3>
+                      {currentQuestionNumber + 1}. {question.questionText}
+                    </h3>
+                    <div className="answerContainer">
+                      {question.answerOptions.map((answer, index) => {
+                        return (
+                          <button key={answer._id} className="answer">
+                            {answer.answerText}
+                          </button>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-        </div>
+                );
+              })}
+          </div>
+        )}
       </div>
     </>
   );
